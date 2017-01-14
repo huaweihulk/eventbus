4994468de6a7c79a4def852a60fc88c44d4f8484
caoyang <15637681569@163.com>
2017-01-14 15:29:58 +0800

    add test disprutor vs guava

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,52 @@
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+
 /**
  * Created by hulk on 17-1-14.
  */
 public class App {
     static class StringObjectHandler extends ObjectEventHandler {
         private int count = 0;
-        
+
+        public int getCount() {
+            return count;
+        }
 
         @Override
         public void handleEvent(Object object, long sequence, boolean endOfBatch) {
-            System.out.println((String) object);
+            //System.err.println((String) object);
+            count++;
+        }
+    }
 
+    static class GuavaEventBus {
+        private int count = 0;
+
+        @Subscribe
+        public void printString(String string) {
+            //System.err.println(string);
+            count++;
         }
     }
 
     public static void main(String[] args) throws EvenHandlerDuplicateException {
-        ObjectEventHandler stringObjectHanlder = new StringObjectHandler();
+        StringObjectHandler stringObjectHanlder = new StringObjectHandler();
         int tag = stringObjectHanlder.getTag();
         DefaultEventBus.instance.registerHandler(stringObjectHanlder);
-        DefaultEventBus.instance.post("hello", tag);
+        long start = 0L;
+        start = System.currentTimeMillis();
+        while (System.currentTimeMillis() - start < 1000) {
+            DefaultEventBus.instance.post("hello", tag);
+        }
+        System.out.println(stringObjectHanlder.getCount());
+
+//        EventBus eventBus = new EventBus();
+//        GuavaEventBus guavaEventBus = new GuavaEventBus();
+//        eventBus.register(guavaEventBus);
+//        start = System.currentTimeMillis();
+//        while ((System.currentTimeMillis() - start) < 1000) {
+//            eventBus.post("hello");
+//        }
+ //       System.out.println(guavaEventBus.count);
     }
 }
